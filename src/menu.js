752b466c70f9a68e6518466d07ba4282f1618a2f
Gabriel <gabrielfunes100202@gmail.com>
2023-03-04 20:46:35 -0300

    bug

--- src/menu.js
+++ src/menu.js
@@ -4,12 +4,16 @@ const menuButton = document.querySelector(".menu-button");
 const subirButton = document.querySelector(".subir-button");
 const abrirMenu = () => {
     const menu = document.querySelector(".menu-mobile");
+    const flecha = document.querySelector(".subir-button");
+    flecha.classList.remove("hidden");
+    flecha.classList.add("block");
     menu.classList.remove("hidden");
     menu.classList.add("flex");
     menuBajar();
 }
 const cerrarMenu = () => {
-    const menu = document.querySelector(".menu-mobile");
+    const flecha = document.querySelector(".subir-button");
+    flecha.classList.add("hidden");
     menuSubir();
 }
 
